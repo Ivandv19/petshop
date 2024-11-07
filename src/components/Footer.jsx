faa39bc8d4a003581f205bd26a6d0c28d778bb29
ivandev <ivangtx19@gmail.com>
2024-11-07 13:04:45 -0600

    refactor: agrega comentarios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,10 @@ import "../assets/css/componentes/footer.css"
 
 const Footer = () => {
     return (
-        <>
-            <footer className="footer">
-                <p>Desarrollado por Alura Latam y por Ivan Cruz <br /> Estudiante ONE (G6) -
-                    Oracle Next Education <br /> 2024 🇲🇽</p>
-            </footer>
-        </>
+        <footer className="footer">
+            <p>Desarrollado por Alura Latam y por Ivan Cruz <br /> Estudiante ONE (G6) -
+                Oracle Next Education <br /> 2024 🇲🇽</p>
+        </footer>
     )
 }
 export default Footer
--- src/routes.js
+++ src/routes.js
@@ -10,11 +10,17 @@ function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Ruta raíz con componente App como contenedor */}
                 <Route path="/" element={<App />}>
+                    {/* Ruta por defecto */}
                     <Route index element={<Home />} />
+                    {/* Ruta para la página "Sobre" */}
                     <Route path="/sobre" element={<Sobre />} />
+                    {/* Ruta dinámica para un post específico */}
                     <Route path="/posts/:id" element={<Post />} />
+                    {/* Ruta dinámica con subrutas para una categoría */}
                     <Route path="/categoria/:id/*" element={<Categoria />} />
+                    {/* Ruta comodín para páginas no encontradas */}
                     <Route path="*" element={<Page404 />} />
                 </Route>
             </Routes>
